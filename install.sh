4e02266d6097a7d5bbb9ebdf22943c3b841601a4
zacmonroe <zmonroe0820@gmail.com>
2024-03-09 19:18:31 -0600

    chore: add installation script

---
+++ install.sh
@@ -0,0 +1,16 @@
+cd ./scripts
+
+echo "Removing old dependencies..."
+rm -rf ./node_modules
+
+echo "Installing new dependencies..."
+mkdir ./node_modules
+cd ./node_modules
+
+# HTMX
+curl https://unpkg.com/htmx.org@1.9.10/dist/htmx.min.js -o ./htmx.min.js
+
+pip install flask
+pip install Jinja2
+
+echo "Done!"
