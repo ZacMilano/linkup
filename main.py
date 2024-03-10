c8a5af593439dfd0ddd4b0ad6041a13ec5e67477
zacmonroe <zmonroe0820@gmail.com>
2024-03-10 15:02:01 -0500

    feat(links): add links for me

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -6,6 +7,25 @@ app = Flask(__name__)
 def hello_world():
   return render_template('index.html')
 
-@app.route('/bingus')
-def bingus():
-  return 'Bingus'
+@app.route('/profile')
+def profile():
+  user = User('Zac', 'zac@quesofres.co', links=[
+    Link('https://quesofres.co', 'Queso Fresco'),
+    Link('https://zacmilano.com', 'Personal site'),
+  ])
+  return render_template('profile.html', user=user)
+
+class User:
+  def __init__(self, username, email, links=[]):
+    self.username = username
+    self.email = email
+    self.links = links
+    self.created_at = datetime.now()
+  
+  def withLink(self, url, title):
+    return Link(url, title)
+
+class Link:
+  def __init__(self, url, title):
+    self.url = url
+    self.title = title
